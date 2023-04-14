6298ef6c60a538e924583a77e0ad48a457c77522
evlev777 <levkovich.evgeniy.98@gmail.com>
2023-04-14 14:33:14 +0300

    feat: complete task simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,24 +5,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  currChain : [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
+    return this.currChain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.currChain.push(`( ${value} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if(this._checkNum(position)){
+      if(position < 1 || position > this.getLength()){
+        this._deleteChain()
+        throw new Error('You can\'t remove incorrect link!')
+      }
+      const index = position - 1;
+      this.currChain.splice(index,1);
+      return this;
+    }
+   this._deleteChain();
+   throw new Error('You can\'t remove incorrect link!')
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.currChain.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const chain = this._getChain();
+    this._deleteChain()
+    return chain;
+  },
+  _deleteChain(){
+    this.currChain.length = 0
+  },
+  _checkNum(num){
+    return typeof num === 'number' && (num ^ 0) === num;
+  },
+  _getChain(){
+    return this.currChain.join('~~');
   }
 };
 
