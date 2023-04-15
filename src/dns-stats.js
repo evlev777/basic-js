427ac11acc7ed0144e30d17d6849a76a8296380e
evlev777 <levkovich.evgeniy.98@gmail.com>
2023-04-15 15:14:53 +0300

    feat:complete task dns stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats( domains ) {
+  const stats = {};
+
+  for (let i = 0; i < domains.length; i++) {
+    const domainParts = domains[i].split('.').reverse();
+    let currentPart = '';
+
+    for (let j = 0; j < domainParts.length; j++) {
+      currentPart += `.${domainParts[j]}`;
+      stats[currentPart] = stats[currentPart] + 1 || 1
+    }
+  }
+
+  return stats;
 }
 
 module.exports = {
